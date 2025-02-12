01dd21589aebc76a32bcf7ecbc86477c0cad7ecb
NavodContrath <navod.contrath@gmail.com>
2025-02-12 14:13:13 +0100

    add second task in bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -14,8 +14,15 @@ const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
-
+const longNames = []
+for (let i = 0; i < teachers.length; i++) {
+  const thisTeacher = teachers[i]
+  console.log(thisTeacher);
+  if (thisTeacher.length >= 5) {
+    longNames.push(thisTeacher)
+  }
+}
+console.log(longNames);
 // 3. Rimuovi 'Ed' dall'array teachers
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
