8c78239b2a0a7540c22b7eb3611ed0e238304875
NavodContrath <navod.contrath@gmail.com>
2025-02-12 13:19:41 +0100

    add patrick instead of Carlo w/splice

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -12,11 +12,9 @@ const teachers = [
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
-
-
-
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+teachers.splice(4, 1, "Patrick")
+console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 
